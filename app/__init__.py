d2414b49251839ecf84bf8f78887138d1b26fcc9
Ebusiness <ebusinesslabs@gmail.com>
2018-07-30 00:28:18 +0300

    έλεγχος σύνδεσης στο πλάνο

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_login import LoginManager
+
+login = LoginManager()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config')
     app.config.from_pyfile('config.py')
-
+    login.init_app(app)
     from .main import bp as main_bp
     app.register_blueprint(main_bp)
 
@@ -12,4 +15,3 @@ def create_app():
     app.register_blueprint(auth_bp)
 
     return app
-
